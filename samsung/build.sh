788aeff48ae94abc81ea01608b0367e133e07bb4
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-11-15 18:50:44 +0100

    ics

--- samsung/build.sh
+++ samsung/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# ARCH LINUX
-if test -e /etc/arch-release ; then
-	if which sudo >/dev/null; then
-		sudo rm -f /usr/bin/python
-		sudo ln -s /usr/bin/python2 /usr/bin/python
-	fi
-fi
-
 START=$(date +%s)
 
 DEVICE="$1"
@@ -25,60 +17,40 @@ case "$DEVICE" in
         exit
         ;;
 	captivatemtd)
-                board=aries
+        board=aries
 		lunch=cyanogen_captivatemtd-eng
 		brunch=captivatemtd
 		;;
 	epic)
-                board=aries
+        board=aries
 		lunch=cyanogen_epic-eng
 		brunch=epic		
         ;;
-	epic4gtouch)
-                board=c1spr
-		lunch=cyanogen_epic4gtouch-eng
-		brunch=epic4gtouch	
-        ;;
 	fascinate)
-                board=aries
+        board=aries
 		lunch=cyanogen_fascinate-eng
 		brunch=fascinate
         ;;
 	galaxys2)
-                board=c1
+        board=c1
 		lunch=cyanogen_galaxys2-eng
 		brunch=galaxys2
 		;;
 	galaxys2att)
-                board=c1att
+        board=c1att
 		lunch=cyanogen_galaxys2att-eng
 		brunch=galaxys2att
 		;;
-	galaxysl)
-                board=latona
-		lunch=cyanogen_galaxysl-eng
-		brunch=galaxysl
-		;;
 	galaxysmtd)
-                board=aries
+        board=aries
 		lunch=cyanogen_galaxysmtd-eng
 		brunch=galaxysmtd
-		;;
-	galaxysbmtd)
-                board=aries
-		lunch=cyanogen_galaxysbmtd-eng
-		brunch=galaxysbmtd
-		;;
-	vibrantmtd)
-                board=aries
-		lunch=cyanogen_vibrantmtd-eng
-		brunch=vibrantmtd
 		;;
 	*)
 		echo "Usage: $0 DEVICE ADDITIONAL"
 		echo "Example: ./build.sh galaxys2 (prebuilt kernel + android)"
 		echo "Example: ./build.sh galaxys2 kernel (kernel + android)"
-		echo "Supported Devices: captivatemtd, epic, epic4gtouch, fascinate, galaxys2, galaxys2att galaxysl, galaxysmtd, galaxysbmtd, vibrantmtd"
+		echo "Supported Devices: captivatemtd, epic, fascinate, galaxys2, galaxys2att galaxysl, galaxysmtd"
 		exit 2
 		;;
 esac
@@ -107,11 +79,3 @@ E_SEC=$((ELAPSED - E_MIN * 60))
 printf "Elapsed: "
 [ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
 printf "%d sec(s)\n" $E_SEC
-
-# ARCH LINUX
-if test -e /etc/arch-release ; then
-	if which sudo >/dev/null; then
-		sudo rm -f /usr/bin/python
-		sudo ln -s /usr/bin/python3 /usr/bin/python
-	fi
-fi
